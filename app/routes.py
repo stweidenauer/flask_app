845b9df349ec53af3cb2301b043e2bac98f7e579
Steffen <stweidenauer@gmail.com>
2020-10-23 11:45:36 +0200

    # 5 camera.html with all pics of one day

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template
 from app import app
 
@@ -15,4 +16,7 @@ def about():
 
 @app.route('/camera')
 def camera():
-    return render_template('camera.html')
+    pic_directory = os.path.join(app.root_path, 'static/2020-10-20')
+    pic_names = os.listdir(pic_directory)
+    print(pic_names)
+    return render_template('camera.html', pic_names=pic_names)
