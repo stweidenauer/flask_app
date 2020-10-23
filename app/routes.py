ed01b61d9351540331aa8bd9dc530482ae95f3ff
Steffen <stweidenauer@gmail.com>
2020-10-23 11:28:10 +0200

    # 4 camera.html with one pic only

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,13 @@ from app import app
 @app.route('/home')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
