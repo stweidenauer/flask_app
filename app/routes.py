cc011fbcca46a1ac12ecb92091a71ebeeae3d11c
Steffen <stweidenauer@gmail.com>
2020-10-24 11:05:45 +0200

    # 6 first try t loop over all pics, NOT working

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,13 @@ def about():
 
 @app.route('/camera')
 def camera():
-    pic_directory = os.path.join(app.root_path, 'static/2020-10-20')
+    # pic_directory = os.path.join(app.root_path, 'static')
+    # all directories with current dates = [r for r in os.listdir(pic_directory) if r.startswith('20')]
+    # Idee Dictonary mit Date als schlüssel und eine liste der Bilder als data
+    # for item in directory with current dates:
+    #     dicy[item] = os.listdir(os.path.join(item))
+    pic_directory = os.path.join(app.root_path, 'static')
     pic_names = os.listdir(pic_directory)
+    pic_names.sort()
     print(pic_names)
     return render_template('camera.html', pic_names=pic_names)
