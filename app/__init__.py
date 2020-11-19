2db437652fda9ca33787250b6cb3858920f8fc07
Steffen <stweidenauer@gmail.com>
2020-11-19 15:34:05 +0100

    # 12 db continue

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xdcfvghjnmk323kmj43nhgvf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from app import routes
