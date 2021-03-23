37c90064d816171700796da2b082330dcf16c978
Steffen <stweidenauer@gmail.com>
2021-03-23 17:02:38 +0100

    # 31 Update improved rki_data

--- app/utilities/rki_data.py
+++ app/utilities/rki_data.py
@@ -4,24 +4,29 @@ url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_La
       "FeatureServer/0/query?where=OBJECTID%3D121&outFields=OBJECTID%2CEWZ%2Clast_update%2C" \
       "death_rate%2Ccases%2Cdeaths%2Ccases_per_100k%2Ccounty%2Ccases7_lk%2Cdeath7_lk%2" \
       "Ccases7_per_100k_txt&returnGeometry=false&f=json"
-response = requests.get(url)
-corona_data = json.loads(response.text)
-h_read = corona_data["features"][0]["attributes"]
+
+
+def update_data():
+    response = requests.get(url)
+    corona_data = json.loads(response.text)
+    h_read = corona_data["features"][0]["attributes"]
+    return h_read
 
 
 def get_incident():
-    return h_read["cases7_per_100k_txt"]
+    return update_data()["cases7_per_100k_txt"]
 
 
 def get_deaths():
-    return h_read["deaths"]
+    return update_data()["deaths"]
 
 
 def get_update():
-    return h_read["last_update"]
+    return update_data()["last_update"]
 
 
 def main():
+    update_data()
     print(F"At {get_update()}")
     print("------------------------")
     print(F"Deaths:\t\t{get_deaths()}")
